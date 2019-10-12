c8d5cc4a57fbcf6285d48c655c049c8927156e73
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2019-10-12 16:21:11 +0100

    Add splash page (#3)

---
+++ index.js
@@ -0,0 +1,20 @@
+function init() {
+  const container = document.createElement('div');
+  container.classList.add('container-main-page');
+  const text = document.createElement('p');
+  text.innerText =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. ' +
+    'Omnis quia iusto cum adipisci repudiandae quo beatae neque. ' +
+    'Quia quis aut aliquid saepe non dolorum deleniti sequi ipsum ex culpa.Accusamus?';
+  const button = document.createElement('button');
+  button.classList.add('button');
+  button.innerText = 'Start Game';
+  button.addEventListener('click', startGame);
+  container.appendChild(text);
+  container.appendChild(button);
+  document.querySelector('.container').appendChild(container);
+}
+
+function startGame() {}
+
+init();
