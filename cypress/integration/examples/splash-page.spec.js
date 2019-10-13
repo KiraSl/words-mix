1bc9a61988a6fea42820d4897b2ff3985af02cc6
Kira Slobodina <42243887+KiraSl@users.noreply.github.com>
2019-10-13 14:53:51 +0100

    Add index and change test (#4)

---
+++ cypress/integration/examples/splash-page.spec.js
@@ -0,0 +1,9 @@
+/// <reference types="Cypress" />
+
+context('Splash page', () => { 
+  it('should render the default html', () => {
+    cy.visit('./index.html');
+    cy.get('.container-main-page .button').should('contain', 'Start Game');
+    cy.get('.container-main-page p').should('contain', 'Lorem ipsum dolor sit amet consectetur adipisicing elit');
+  });
+});
