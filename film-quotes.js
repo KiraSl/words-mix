a8d34b942d0c38f94e60ee04eba74a40310eb337
Kira Slobodina <slobodina.kira@gmail.com>
2019-10-16 20:43:30 +0100

    Add film-quotes files

---
+++ film-quotes.js
@@ -0,0 +1,530 @@
+/* eslint-disable quotes*/
+/* eslint-disable max-len*/
+/* eslint-disable no-unused-vars*/
+const quotes = [
+  {
+    quote: "Frankly, my dear, I don't give a damn.",
+    filmTitle: 'Gone With the Wind',
+    year: '1939'
+  },
+  {
+    quote: "I'm going to make him an offer he can't refuse.",
+    filmTitle: 'The Godfather',
+    year: '1972 ',
+    funFact: 'This line makes it into each Godfather film in some way or another.'
+  },
+  {
+    quote:
+      "You don't understand! I coulda had class. I coulda been a contender. I could've been somebody, instead of a bum, which is what I am.",
+    filmTitle: 'On the Waterfront',
+    year: '1954'
+  },
+  {
+    quote: "Toto, I've got a feeling we're not in Kansas anymore.",
+    filmTitle: 'The Wizard of Oz',
+    year: '1939',
+    funFact:
+      'As one of the most famous movie quotes in film history, this line has been parodied by many different movies and television shows.  '
+  },
+  {
+    quote: "Here's looking at you, kid.",
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: 'Go ahead, make my day.',
+    filmTitle: 'Sudden Impact',
+    year: '1983'
+  },
+  {
+    quote: "All right, Mr. DeMille, I'm ready for my close-up.",
+    filmTitle: 'Sunset Blvd.',
+    year: '1950Han Solo saying may the force be with you'
+  },
+  {
+    quote: 'May the Force be with you.',
+    filmTitle: 'Star Wars',
+    year: '1977'
+  },
+  {
+    quote: "Fasten your seatbelts. It's going to be a bumpy night.",
+    filmTitle: 'All About Eve',
+    year: '1950'
+  },
+  {
+    quote: 'You talking to me? ',
+    filmTitle: 'Taxi Driver',
+    year: '1976',
+    funFact:
+      "Robert DeNiro improvised this line. The script only said 'Travis speaks to himself in the mirror' so DeNiro took some liberties and was ultimately successful."
+  },
+  {
+    quote: "What we've got here is failure to communicate.",
+    filmTitle: 'Cool Hand Luke',
+    year: '1967'
+  },
+  {
+    quote: 'I love the smell of napalm in the morning.',
+    filmTitle: 'Apocalypse Now',
+    year: '1979'
+  },
+  {
+    quote: "Love means never having to say you're sorry.",
+    filmTitle: 'Love Story',
+    year: '1970',
+    funFact:
+      "Two years later Ryan O'Neal will hear this line again in the film What's Up Doc when Barbara Streisand says it to his character and respond, 'That's the dumbest thing I've ever heard.'"
+  },
+  {
+    quote: 'The stuff that dreams are made of.',
+    filmTitle: 'The Maltese Falcon',
+    year: '1941'
+  },
+  {
+    quote: 'E.T. phone home.',
+    filmTitle: 'E.T. The Extra-Terrestrial',
+    year: '1982'
+  },
+  {
+    quote: 'They call me Mister Tibbs! ',
+    filmTitle: 'In the Heat of the Night',
+    year: '1967'
+  },
+  {
+    quote: 'Rosebud.',
+    filmTitle: 'Citizen Kane',
+    year: '1941'
+  },
+  {
+    quote: 'Made it, Ma! Top of the world! ',
+    filmTitle: 'White Heat',
+    year: '1949'
+  },
+  {
+    quote: "I'm as mad as hell, and I'm not going to take this anymore! ",
+    filmTitle: 'Network',
+    year: '1976'
+  },
+  {
+    quote: 'Louis, I think this is the beginning of a beautiful friendship.',
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: 'A census taker once tried to test me. I ate his liver with some fava beans and a nice Chianti.',
+    filmTitle: 'The Silence of the Lambs',
+    year: '1991'
+  },
+  {
+    quote: 'Bond. James Bond.',
+    filmTitle: 'Dr. No',
+    year: '1962'
+  },
+  {
+    quote: "There's no place like home.",
+    filmTitle: 'The Wizard of Oz',
+    year: '1939'
+  },
+  {
+    quote: "I am big! It's the pictures that got small.",
+    filmTitle: 'Sunset Blvd.',
+    year: '1950'
+  },
+  {
+    quote: 'Show me the money! ',
+    filmTitle: 'Jerry Maguire',
+    year: '1996'
+  },
+  {
+    quote: "Why don't you come up sometime and see me? ",
+    filmTitle: 'She Done Him Wrong',
+    year: '1933'
+  },
+  {
+    quote: "I'm walking here! I'm walking here! ",
+    filmTitle: 'Midnight Cowboy',
+    year: '1969',
+    funFact:
+      'This scene was improvised. Midnight Cowboy filmed in New York City so the taxi in the shot is an actual New York taxi. Remaining in character Hoffman yells at the driver and the scene made the cut in the film.'
+  },
+  {
+    quote: "Play it, Sam. Play 'As Time Goes By.' ",
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: "You can't handle the truth! ",
+    filmTitle: 'A Few Good Men',
+    year: '1992'
+  },
+  {
+    quote: 'I want to be alone.',
+    filmTitle: 'Grand Hotel',
+    year: '1932'
+  },
+  {
+    quote: 'After all, tomorrow is another day! ',
+    filmTitle: 'Gone With the Wind',
+    year: '1939'
+  },
+  {
+    quote: 'Round up the usual suspects.',
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: "I'll have what she's having.",
+    filmTitle: 'When Harry Met Sally',
+    year: '1989'
+  },
+  {
+    quote: "You know how to whistle, don't you, Steve? You just put your lips together and blow.",
+    filmTitle: 'To Have and Have Not',
+    year: "1944Jaws, you're gonna need a bigger boat"
+  },
+  {
+    quote: "You're gonna need a bigger boat.",
+    filmTitle: 'Jaws',
+    year: '1975'
+  },
+  {
+    quote: "Badges? We ain't got no badges! We don't need no badges! I don't have to show you any stinking badges! ",
+    filmTitle: 'The Treasure of the Sierra Madre',
+    year: '1948'
+  },
+  {
+    quote: "I'll be back.",
+    filmTitle: 'The Terminator',
+    year: '1984'
+  },
+  {
+    quote: 'Today, I consider myself the luckiest man on the face of the earth.',
+    filmTitle: 'The Pride of the Yankees',
+    year: '1942'
+  },
+  {
+    quote: 'If you build it, he will come.',
+    filmTitle: 'Field of Dreams',
+    year: '1989'
+  },
+  {
+    quote: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+    filmTitle: 'Forrest Gump',
+    year: '1994'
+  },
+  {
+    quote: 'We rob banks.',
+    filmTitle: 'Bonnie and Clyde',
+    year: '1967'
+  },
+  {
+    quote: 'Plastics.',
+    filmTitle: 'The Graduate',
+    year: '1967'
+  },
+  {
+    quote: "We'll always have Paris.",
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: 'I see dead people.',
+    filmTitle: 'The Sixth Sense',
+    year: '1999',
+    funFact:
+      "When Haley Joel Osmet says this line the camera goes to Bruce Willis's face. This is a cinematic clue that Bruce Willis's character is dead.Stella, Hey Stella scene Streetcar"
+  },
+  {
+    quote: 'Stella! Hey, Stella! ',
+    filmTitle: 'A Streetcar Named Desire',
+    year: '1951'
+  },
+  {
+    quote: "Oh, Jerry, don't let's ask for the moon. We have the stars.",
+    filmTitle: 'Now, Voyager',
+    year: '1942'
+  },
+  {
+    quote: 'Shane. Shane. Come back! ',
+    filmTitle: 'Shane',
+    year: '1953'
+  },
+  {
+    quote: "Well, nobody's perfect.",
+    filmTitle: 'Some Like It Hot',
+    year: '1959'
+  },
+  {
+    quote: "It's alive! It's alive! ",
+    filmTitle: 'Frankenstein',
+    year: '1931',
+    funFact:
+      "The original line was 'It's alive! It's alive! In the name of God! Now I know what it's like to be God!'​​​ Censors cut Dr. Frankenstein's full line because it was considered sacrilege."
+  },
+  {
+    quote: 'Houston, we have a problem.',
+    filmTitle: 'Apollo 13',
+    year: '1995'
+  },
+  {
+    quote: "You've got to ask yourself one question: 'Do I feel lucky?' Well, do ya, punk? ",
+    filmTitle: 'Dirty Harry',
+    year: '1971'
+  },
+  {
+    quote: 'You had me at ‘hello.’ ',
+    filmTitle: 'Jerry Maguire',
+    year: '1996'
+  },
+  {
+    quote: "One morning I shot an elephant in my pajamas. How he got in my pajamas, I don't know.",
+    filmTitle: 'Animal Crackers',
+    year: '1930league of their own, theres no crying in baseball'
+  },
+  {
+    quote: "There's no crying in baseball! ",
+    filmTitle: 'A League of Their Own',
+    year: '1992'
+  },
+  {
+    quote: 'La-dee-da, la-dee-da.',
+    filmTitle: 'Annie Hall',
+    year: '1977'
+  },
+  {
+    quote: "A boy's best friend is his mother.",
+    filmTitle: 'Psycho',
+    year: '1960'
+  },
+  {
+    quote: 'Greed, for lack of a better word, is good.',
+    filmTitle: 'Wall Street',
+    year: '1987'
+  },
+  {
+    quote: 'Keep your friends close, but your enemies closer.',
+    filmTitle: 'The Godfather II',
+    year: '1974'
+  },
+  {
+    quote: "As God is my witness, I'll never be hungry again.",
+    filmTitle: 'Gone With the Wind',
+    year: '1939'
+  },
+  {
+    quote: "Well, here's another nice mess you've gotten me into! ",
+    filmTitle: 'Sons of the Desert',
+    year: '1933Scarface say hello to my little friend scene'
+  },
+  {
+    quote: 'Say hello to my little friend!',
+    filmTitle: 'Scarface',
+    year: '1983'
+  },
+  {
+    quote: 'What a dump.',
+    filmTitle: 'Beyond the Forest',
+    year: '1949'
+  },
+  {
+    quote: "Mrs. Robinson, you're trying to seduce me. Aren't you? ",
+    filmTitle: 'The Graduate',
+    year: '1967'
+  },
+  {
+    quote: "Gentlemen, you can't fight in here! This is the War Room!",
+    filmTitle: 'Dr.Strangelove',
+    year: '1964'
+  },
+  {
+    quote: 'Elementary, my dear Watson.',
+    filmTitle: 'The Adventures of Sherlock Holmes',
+    year: '1929',
+    funFact: 'Sherlock Holmes never says this iconic quote in any of the books the character is based off of.'
+  },
+  {
+    quote: 'Get your stinking paws off me, you damned dirty ape.',
+    filmTitle: 'Planet of the Apes',
+    year: '1968'
+  },
+  {
+    quote: 'Of all the gin joints in all the towns in all the world, she walks into mine.',
+    filmTitle: 'Casablanca',
+    year: '1942'
+  },
+  {
+    quote: "Here's Johnny! ",
+    filmTitle: 'The Shining',
+    year: '1980',
+
+    funFact:
+      'This line was completely improvised. Jack Nicholson was only prompted to break down the door, but he decided to improvise this line and it made the final cut of the movie.'
+  },
+  {
+    quote: "They're here! ",
+    filmTitle: 'Poltergeist',
+    year: '1982'
+  },
+  {
+    quote: 'Is it safe? ',
+    filmTitle: 'Marathon Man',
+    year: '1976'
+  },
+  {
+    quote: "Wait a minute, wait a minute. You ain't heard nothin' yet! ",
+    filmTitle: 'The Jazz Singer',
+    year: '1927'
+  },
+  {
+    quote: 'No wire hangers, ever! ',
+    filmTitle: 'Mommie Dearest',
+    year: '1981'
+  },
+  {
+    quote: 'Mother of mercy, is this the end of Rico? ',
+    filmTitle: 'Little Caesar',
+    year: '1930'
+  },
+  {
+    quote: "Forget it, Jake, it's Chinatown.",
+    filmTitle: 'Chinatown',
+    year: '1974'
+  },
+  {
+    quote: 'I have always depended on the kindness of strangers.',
+    filmTitle: 'A Streetcar Named Desire',
+    year: '1951Terminator saying hasta la vista baby'
+  },
+  {
+    quote: 'Hasta la vista, baby.',
+    filmTitle: 'Terminator 2: Judgment Day',
+    year: '1991​​​​​'
+  },
+  {
+    quote: 'Soylent Green is people! ',
+    filmTitle: 'Soylent Green',
+    year: '1973'
+  },
+  {
+    quote: 'Open the pod bay doors, HAL.',
+    filmTitle: '2001: A Space Odyssey',
+    year: '1968'
+  },
+  {
+    quote: "Surely you can't be serious.",
+    filmTitle: "Rumack: I am serious…and don't call me Shirley. Airplane!",
+    year: '1980'
+  },
+  {
+    quote: 'Yo, Adrian! ',
+    filmTitle: 'Rocky',
+    year: '1976Rocky saying yo Adrian'
+  },
+  {
+    quote: 'Hello, gorgeous.',
+    filmTitle: 'Funny Girl',
+    year: '1968'
+  },
+  {
+    quote: 'Toga! Toga! ',
+    filmTitle: "National Lampoon's Animal House",
+    year: '1978'
+  },
+  {
+    quote: 'Listen to them. Children of the night. What music they make.',
+    filmTitle: 'Dracula',
+    year: '1931'
+  },
+  {
+    quote: "Oh, no, it wasn't the airplanes. It was Beauty killed the Beast.",
+    filmTitle: 'King Kong',
+    year: '1933',
+
+    funFact: "The King Kong roar was a combination of a lion and tiger's roar combined and played backward very slowly."
+  },
+  {
+    quote: 'My precious.',
+    filmTitleP: 'The Lord of the Rings: Two Towers',
+    year: '2002'
+  },
+  {
+    quote: 'Attica! Attica! ',
+    filmTitle: 'Dog Day Afternoon',
+    year: '1975'
+  },
+  {
+    quote: "Sawyer, you're going out a youngster, but you've got to come back a star! ",
+    filmTitle: '42nd Street',
+    year: '1933'
+  },
+  {
+    quote:
+      "Listen to me, mister. You're my knight in shining armor. Don't you forget it. You're going to get back on that horse, and I'm going to be right behind you, holding on tight, and away we're gonna go, go, go! ",
+    filmTitle: 'On Golden Pond',
+    year: '1981'
+  },
+  {
+    quote: "Tell 'em to go out there with all they got and win just one for the Gipper.",
+    filmTitle: 'Knute Rockne All American',
+    year: '1940'
+  },
+  {
+    quote: 'A martini. Shaken, not stirred.',
+    filmTitle: 'Goldfinger',
+    year: '1964'
+  },
+  {
+    quote: "Who's on first.",
+    filmTitle: 'The Naughty Nineties',
+    year: '1945'
+  },
+  {
+    quote:
+      "Cinderella story. Outta nowhere. A former greenskeeper, now, about to become the Masters champion. It looks like a mirac...It's in the hole! It's in the hole! It's in the hole! ",
+    filmTitle: 'Caddyshack',
+    year: '1980'
+  },
+  {
+    quote: 'Life is a banquet, and most poor suckers are starving to death! ',
+    filmTitle: 'Auntie Mame',
+    year: '1958'
+  },
+  {
+    quote: 'I feel the need - the need for speed! ',
+    filmTitle: 'Top Gun',
+    year: '1986'
+  },
+  {
+    quote: 'Carpe diem. Seize the day, boys. Make your lives extraordinary.',
+    filmTitle: 'Dead Poets Society',
+    year: '1989',
+    funFact:
+      "The inspiration for Robin William's character, John Keating, is Samuel Pickering, who most recently worked as an English Professor at the University of Connecticut."
+  },
+  {
+    quote: 'Snap out of it! ',
+    filmTitle: 'Moonstruck',
+    year: '1987'
+  },
+  {
+    quote: 'My mother thanks you. My father thanks you. My sister thanks you. And I thank you.',
+    filmTitle: 'Yankee Doodle Dandy',
+    year: '1942'
+  },
+  {
+    quote: 'Nobody puts Baby in a corner.',
+    filmTitle: 'Dirty Dancing',
+    year: '1987'
+  },
+  {
+    quote: "I'll get you, my pretty, and your little dog, too! ",
+    filmTitle: 'The Wizard of Oz',
+    year: "1939Jack Dawson, Titanic, yelling I'm the King of the world"
+  },
+  {
+    quote: "I'm king of the world! ",
+    filmTitle: 'Titanic',
+    year: '1997'
+  }
+];
+// export default quotes;
