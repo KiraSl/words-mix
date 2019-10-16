752a76c639a63a040bd1d63888595aa4a4152aed
Kira Slobodina <slobodina.kira@gmail.com>
2019-10-16 20:42:15 +0100

    Add cypress test

--- test/tests.js
+++
@@ -1,9 +0,0 @@
-const { randomiseSentence } = require('../index');
-
-QUnit.test('hello test', function(assert) {
-  const sentence = 'Just some random sentence';
-  const result = randomiseSentence(sentence);
-  assert.ok(Array.isArray(result), 'Function return array');
-  assert.equal(result.length, sentence.split(' ').length, 'Array has correct nr of items');
-  assert.notDeepEqual(result, sentence.split(' '), 'Randomise function works correctly');
-});
